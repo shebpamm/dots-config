9af0a54b7d6ba98cd34d707e2dd8ed82cdd4e39c
Erik Karsten <erik@opcode.xyz>
2021-07-30 15:59:25 +0300

    Made startx autoexec when logging in.

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.config/fish/aliases.fish
     source ~/.config/fish/herbst_completion.fish
+
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
 end
+
+# Source some env vars that are private and not to be committed, such as tokens etc...
+source ~/.config/fish/private_variables
